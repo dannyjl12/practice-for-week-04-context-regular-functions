f8d2ca1490d23f351b359aa0caa6480c005366f2
danny-lowton <dannylowton@gmail.com>
2023-06-17 17:30:42 -0400

    completed

--- context.js
+++ context.js
@@ -15,23 +15,23 @@ class Fish {
 const nemo = new Fish('Nemo');
 
 /********************************* Scenario 1 *********************************/
-// eat(); // ?
+ eat(); // undefined eats fish food - eat is defined in global space so does not take in any name, therefore it is undefined - correct
 
 
 /********************************* Scenario 2 *********************************/
-// nemo.eat = eat;
-// nemo.eat(); // ?
+nemo.eat = eat;
+nemo.eat(); // Nemo eats fish food - nemo is defined, therefore it allows this to be nemo - correct
 
 
 /********************************* Scenario 3 *********************************/
-// nemo.eat = eat;
-// eat(); // ?
+nemo.eat = eat;
+eat(); // undefined eats fish food - eat is in the global space so redifining nemo.eat as eat doesn't change the global space - correct
 
 
 /********************************* Scenario 4 *********************************/
-// nemo.swim(); // ?
+nemo.swim(); // nemo swimming in the water - swim is a method so defining nemo as a new fish allows access to the swim method - correct
 
 
 /********************************* Scenario 5 *********************************/
-// const swim = nemo.swim;
-// swim(); // ?
+const swim = nemo.swim;
+swim(); // type error - defininig swim as nemo.swim does not allow it access to the swim method as it refers to the global space - correct
